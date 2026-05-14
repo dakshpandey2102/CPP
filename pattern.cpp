cec9dd27d6b7ec6a43a8e129d3bd0f9e806f7abe
Daksh Pandey <dakshpandey748@gmail.com>
2026-05-14 23:28:30 +0530

    new

---
+++ pattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //  rectangle banana hai 
+    // rows -> m, cols -> 5
+    int m;
+    cout<<"enter number of rows";
+    cin>>m;
+    int n;
+    cout<<"enter number of cols :";
+    cin>> n;
+    for(int i=1;i<=m;i++){// rows = m
+        for(int i =1;i<=n;i++){//cols= n
+            cout<<"*"; 
+        }
+        cout<<endl;
+    }
+}
