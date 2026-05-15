f3427a2c78c765e9a5765c7ed501653d155a5eb9
Daksh Pandey <dakshpandey748@gmail.com>
2026-05-15 14:25:52 +0530

    learning

---
+++ alphabetsquare.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< " enter n:";
+    cin>> n;
+    int m;
+    cout<< " enter m:";
+    cin>> m;
+    for(char i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            cout<< (char)(j+64)<<" ";
+
+        }
+        cout<< endl;
+    }
+    
+}
