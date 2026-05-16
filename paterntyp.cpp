e3533b70e078d52543518ad16dbe5c236486773f
Daksh Pandey <dakshpandey748@gmail.com>
2026-05-16 17:22:39 +0530

    new re

--- paterntyp.cpp
+++ paterntyp.cpp
@@ -1,1 +1,38 @@
-#include<
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<< "enter n:";
+//     cin>> n;
+//     int  m;
+//     cout<< " enter m:";
+//     cin>> m;
+//     for (int i=1; i<=m;i++){
+//         for(int j=i;j<=n;j++){
+//             cout<<i<<" ";
+
+//         }
+//         cout<< endl;
+//     }
+// }
+
+// or
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "enter n:";
+    cin>> n;
+    int  m;
+    cout<< " enter m:";
+    cin>> m;
+    for (int i=1; i<=m;i++){
+        for(int j=1;j<=n-i+1;j++){
+            cout<<i<<" ";
+
+        }
+        cout<< endl;
+    }
+}
