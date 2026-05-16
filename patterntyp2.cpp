8927d917427cc9c101c5f812408a51fd45a5f5ed
Daksh Pandey <dakshpandey748@gmail.com>
2026-05-16 17:35:33 +0530

    new

---
+++ patterntyp2.cpp
@@ -0,0 +1,22 @@
+//4
+//4 3
+//4 3 2
+//4 3 2 1
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "enter n:";
+    cin>> n;
+    int  m;
+    cout<< " enter m:";
+    cin>> m;
+    for (int i=1; i<=m;i++){
+        for(int j=1;j<=i;j++){
+            cout<<n-j+1<<" ";
+
+        }
+        cout<< endl;
+    }
+}
