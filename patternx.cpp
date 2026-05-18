5fa0e1947de0af4aeac8cae53287d2a9bd179e37
Daksh Pandey <dakshpandey748@gmail.com>
2026-05-18 13:19:20 +0530

    new

---
+++ patternx.cpp
@@ -0,0 +1,29 @@
+// 1
+// 0 1 
+// 1 0 1
+// 0 1 0 1
+
+// logic -
+// (i+j)%2 == 0 -> 1
+//  else        -> 0
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< " enter numbber of rows : ";
+    cin>>  n;
+    int m;
+    cout<< " enter m:";
+    cin>> m;
+    for (int i=1; i<=m;i++){
+        for(int j=1;j<=i;j++){
+            if( (i+j)%2==0){
+                cout<< 1<<" ";
+            }else{
+                cout<<0<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
