b2598562d1ffc279efc504892d79396a61e3db98
Daksh Pandey <dakshpandey748@gmail.com>
2026-05-18 12:43:46 +0530

    new

---
+++ floydstriang.cpp
@@ -0,0 +1,24 @@
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "enter n:";
+    cin>> n;
+    int  m;
+    cout<< " enter m:";
+    cin>> m;
+    int a=1;
+    for (int i=1; i<=m;i++){
+        for(int j=1;j<=i;j++){
+            cout<<a<<" ";
+            a+=1;
+
+        }
+        cout<< endl;
+    }
+}
--- patterntyp4.cpp
+++ patterntyp4.cpp
@@ -22,5 +22,19 @@ int main(){
         }
         cout<<endl;
     }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<< 2*j-1<< " ";
+        }
+        cout<<endl;
+    }
+    for(int i=1;i<=n;i++){
+        int a =1;
+        for(int j=1;j<=i;j++){
+            cout<<a<<" ";
+            a+=2;
+        }
+        cout<<endl;
+    }
 
 }
