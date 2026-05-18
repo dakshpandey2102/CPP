f69bcb5cc8ff7c0d9003d4af6f954a86d67de8a1
Daksh Pandey <dakshpandey748@gmail.com>
2026-05-18 13:39:08 +0530

    new learn

---
+++ starplus.cpp
@@ -0,0 +1,25 @@
+//   1 2 3 4 5 ->j
+// 1  # # * # #
+// 2 # # * # #
+// 3 * * * * *
+// 4 # # * # #
+// 5 # # * # #
+// |-i
+// n=5
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< " enter no. of rows : ";
+    cin>>n;
+    int mid = n/2 +1;
+    for (int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==mid || j==mid) cout<<"*";
+            else cout<<" ";
+        }
+        cout<<endl;
+    }
+
+}
