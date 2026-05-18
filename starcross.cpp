359b807523b1e4e0296ec88c5a4e10012803f203
Daksh Pandey <dakshpandey748@gmail.com>
2026-05-18 14:41:39 +0530

    new

--- starcross.cpp
+++ starcross.cpp
@@ -1,12 +1,10 @@
 //   1 2 3 4 5 ->j
-// 1 # # * # #
-// 2 # # * # #
-// 3 * * * * *
-// 4 # # * # #
-// 5 # # * # #
-// |-i
-// n=5
-
+//1  * # # # *
+//2  # * # * #
+//3  # # * # #
+//4  # * # * #
+//5  * # # # *
+//i
 #include<iostream>
 using namespace std;
 int main(){
@@ -16,7 +14,7 @@ int main(){
     int mid = n/2 +1;
     for (int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            if(i==mid || j==mid) cout<<"*";
+            if(i==j || i+j== n+1) cout<<"*";
             else cout<<" ";
         }
         cout<<endl;
