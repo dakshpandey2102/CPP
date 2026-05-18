cffd84050e4c0c31f3b11a3596ac4ea258601056
Daksh Pandey <dakshpandey748@gmail.com>
2026-05-18 14:21:02 +0530

    new learn

---
+++ hollowrectangle.cpp
@@ -0,0 +1,24 @@
+//******
+//*    *
+//*    *
+//****** 
+#include<iostream>
+using namespace std;
+int main(){
+    // int m;
+    // cout<< " enter m";
+    // cin>> m;
+    int n;
+    cout<< " enter n:";
+    cin>> n;
+    for(int i=1; i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==n || j== n || i==1||j==1){
+            cout<<"*";
+            }else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
