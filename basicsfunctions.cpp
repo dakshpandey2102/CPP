a8f540da0af9892086e31429e14a77086b51eca4
Daksh Pandey <dakshpandey748@gmail.com>
2026-05-21 22:51:06 +0530

    new

--- basicsfunctions.cpp
+++ basicsfunctions.cpp
@@ -1,11 +1,25 @@
+// #include<iostream>
+// using namespace std;
+// void greet(){
+//     cout<<"Good Morning"<<endl;
+//     cout<<"How are you?"<<endl;
+// }
+// int main(){
+//     greet();
+//     cout<<"Hey"<<endl;
+//     greet() ;
+// }
+
 #include<iostream>
 using namespace std;
-void greet(){
-    cout<<"Good Morning"<<endl;
-    cout<<"How are you?"<<endl;
+void usa(){
+    cout<<"You are in USA"<<endl;
+}
+void india(){
+    cout<<" you are in India"<<endl;
 }
 int main(){
-    greet();
-    cout<<"Hey"<<endl;
-    greet() ;
+    usa();
+    cout<<"You are in main"<<endl;
+    india();
 }
