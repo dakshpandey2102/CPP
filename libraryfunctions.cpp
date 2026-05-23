3e0ccf1060120bd6c8dba1af45c996ca4c796517
Daksh Pandey <dakshpandey748@gmail.com>
2026-05-23 23:33:58 +0530

    new

---
+++ libraryfunctions.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n;
+    cout<< "enter n";
+    cin >> n;
+
+    cout<<sqrt(n);
+    cout <<" cube root"<< endl;
+    cout << cbrt(n);
+}
