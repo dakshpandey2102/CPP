d4b1657f5382c6b444dbc866b8d0d86bb63112d2
Daksh Pandey <dakshpandey748@gmail.com>
2026-05-23 23:59:36 +0530

    new

---
+++ combination.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    int r;
+    cout<<"Enter r : ";
+    cin>>r;
+    int a = 1;
+    for(int i =1;i<=n;i++){
+        a*=i;
+
+    }
+    int  b=1;
+    for(int i=1;i<=r;i++){
+        b *= i
+    }
+}
