86127f09e3328579d2fe50551089720973f2a5b4
Daksh Pandey <dakshpandey748@gmail.com>
2026-05-24 13:04:58 +0530

    new

--- swapfunction.cpp
+++ swapfunction.cpp
@@ -1,6 +1,27 @@
+// pass by value 
+// #include<iostream>
+// using namespace std;
+// void  swap(int x, int y){// diffrent part
+//     int temp = x;
+//     x =y;
+//     y = temp;
+
+// }
+// int main(){
+//     int x= 12;
+//     int y = 45;
+//     cout<< x<<" "<<y<<endl;
+//     swap(x,y);
+//     cout<<x<<" "<<y<<endl;
+// }
+
+
+
+
+// pass by refrence
 #include<iostream>
 using namespace std;
-void  swap(int& x, int& y){
+void  swap(int& x, int& y){//  diffrent part 
     int temp = x;
     x =y;
     y = temp;
