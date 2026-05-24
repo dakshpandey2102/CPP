470092c904e74930daae878a560f5ea00f5acf9b
Daksh Pandey <dakshpandey748@gmail.com>
2026-05-24 12:14:00 +0530

    new

---
+++ scope.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "enter n:";
+    cin>> n;
+    int i;
+    for(int i =1;i<=5;i++){
+        cout<<i<<endl;
+    }
+    cout<<i <<endl;
+
+}
