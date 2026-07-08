a631c7ba3893f5ca39a7293784a2b1a6db11e8f2
Daksh Pandey <dakshpandey748@gmail.com>
2026-07-09 00:21:29 +0530

    new

---
+++ pat4st.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Solution{
+    public:
+    void pattern4n(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<< i;
+            }cout<< endl;
+        }
+    }
+};
+ 
+int main(){
+    int n;
+    cin >> n;
+    Solution obj;
+    obj.pattern4n(n);
+
+}
