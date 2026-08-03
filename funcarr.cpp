f1f7fcf4dbf1c7cdb69b69075bb641820622b8a3
Daksh Pandey <dakshpandey748@gmail.com>
2026-08-03 19:45:12 +0530

    new

--- funcarr.cpp
+++ funcarr.cpp
@@ -16,4 +16,19 @@ int main(){
     int third [15] = {2,7};
     // int n = 15;
     printArray(third, 15);
+    // intialising all locations with 0
+    int fourth [10] = {0};
+    
+    // int n=10;
+    printArray(fourth, 10);
+
+    // initialising all locations with 1 [ not possible below the line ]
+    int fifth [10]= {1};
+
+    int n=10;
+    printArray(fifth, 10);
+
+    int fifthSize = sizeof(fifth)/sizeof(int);
+    cout<< "Size of Fifth is " << fifthSize <<endl;
+
 }
