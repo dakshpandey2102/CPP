a177469a95d9a733785e83d5a8643d6be2ed4e2f
Daksh Pandey <dakshpandey748@gmail.com>
2026-08-04 23:34:37 +0530

    new

---
+++ MaxMin.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include <climits>
+using namespace std;
+
+
+int getMin (int num[], int n){
+    int min = INT_MAX;
+
+    for(int i=0;i<n;i++){
+        if(num[i]< min){
+            min = num[i];
+        }
+    }
+
+    // returning max value 
+    return min;
+}
+
+int getMax(int num[], int n){
+
+    int max = INT_MIN;
+    for(int i =0; i<n; i++){
+        if(num[i]>max){
+            max = num[i];
+        }
+    }
+
+    //returning max value
+    return max;
+
+
+}
+int main(){
+    int size;
+    cin >> size;
+
+    int num[100];
+    // taking input in array
+    for(int i=0;i<size;i++){
+        cin >> num[i];
+    }
+    cout<< " Maximum value is " << getMax(num, size) << endl;
+
+    return 0;
+}
